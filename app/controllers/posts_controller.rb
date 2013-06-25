61830cc00275c1a0f0100a756a5c06e2556e8854
MohammedShehata <shehata.mohammed@gmail.com>
2013-06-25 23:06:40 +0200

    delete by ajax and juery

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -63,7 +63,7 @@ class PostsController < ApplicationController
     @post = Post.find params[:id]
     @post.destroy
     
-    redirect_to posts_path
+    # redirect_to posts_path
   end
   
   def like
